ad36eb730893a87d7a9ed62a778417bbc84bd450
Ahmed Ghlimi <aghlimi@c3r2p4.1337.ma>
2024-11-12 12:09:01 +0100

    f

---
+++ monitoring.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+total=$(free -mt|grep ^Total|awk '{print $2}');
+used=c;
+prsnt=$(awk "BEGIN {printf \"%.2f\", ($used / $total) * 100}")
+
+echo "
+Broadcast message from root@wil (tty1) (Sun Apr 25 15:45:00 2021):
+        #Architecture: $(uname -a)
+        #CPU physical : $(lscpu | grep -c '^Core(s) per socket:')
+        #vCPU : $(lscpu | grep -c '^CPU(s):')
+        #Memory Usage: $(free -mt|grep ^Total|awk '{print $2}')/$(free -mt|grep ^Total|awk '{print $2}')MB ($(awk "BEGIN {printf \"%.2f\", ($used / $total) * 100}")%)
+        #Disk Usage: 1009/2Gb (49%)
+        #CPU load: 6.7%
+        #Last boot: 2021-04-25 14:45
+        #LVM use: yes
+        #Connections TCP : 1 ESTABLISHED
+        #User log: 1
+        #Network: IP 10.0.2.15 (08:00:27:51:9b:a5)
+        #Sudo : 42 cmd
+"
