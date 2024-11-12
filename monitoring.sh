457a4a32b17e6e005cc3311f977488399c211a7e
Ahmed Ghlimi <aghlimi@c3r2p4.1337.ma>
2024-11-12 13:30:45 +0100

    k

--- monitoring.sh
+++ monitoring.sh
@@ -2,14 +2,13 @@
 total=$(free -b|grep ^Mem|awk '{print $2}');
 used=$(free -b|grep ^Mem|awk '{print $3}');
 mem=$(awk "BEGIN {printf \"%.2f\", ($used / $total) * 100}")
-
 echo "
 Broadcast message from root@wil (tty1) (Sun Apr 25 15:45:00 2021):
         #Architecture: $(uname -a)
         #CPU physical : $(lscpu | grep -c '^Core(s) per socket:')
         #vCPU : $(lscpu | grep -c '^CPU(s):')
         #Memory Usage: $(free -h|grep ^Mem|awk '{print $3}')/$(free -h|grep ^Mem|awk '{print $2}') ($mem%)
-        #Disk Usage: 1009/2Gb (49%)
+        #Disk Usage: $(df -m --total|grep total | awk '{print $3}')/$(df -h --total|grep total | awk '{print $2}')b ($(df -h --total|grep total | awk '{print $5}'))
         #CPU load: 6.7%
         #Last boot: 2021-04-25 14:45
         #LVM use: yes
